c1c748f78cc90cbc77780dbb1e24301e4427f13b
mitaniHub <106486617+mitaniHub@users.noreply.github.com>
2022-05-29 20:26:11 +0300

    Add files via upload

---
+++ DeleteBinObj.bat
@@ -0,0 +1,15 @@
+echo "Deleting Folder - Gateway"
+@RD /S /Q "C:\HandyCodes\Middleware\APIGatewayDemo\bin"
+@RD /S /Q "C:\HandyCodes\Middleware\APIGatewayDemo\obj"
+
+echo "Deleting Folder - CustomersAPIServices"
+@RD /S /Q "C:\HandyCodes\Middleware\CustomersAPIServices\bin"
+@RD /S /Q "C:\HandyCodes\Middleware\CustomersAPIServices\obj"
+
+echo "Deleting Folder - JWTAuthentication"
+@RD /S /Q "C:\HandyCodes\Middleware\Login_JWT\bin"
+@RD /S /Q "C:\HandyCodes\Middleware\Login_JWT\obj"
+
+echo "Deleting Folder - ProductsAPIServices"
+@RD /S /Q "C:\HandyCodes\Middleware\ProductsAPIServices\bin"
+@RD /S /Q "C:\HandyCodes\Middleware\ProductsAPIServices\obj"
---
+++ RunAllServices.bat
@@ -0,0 +1,9 @@
+cd "C:\HandyCodes\Middleware\Login_JWT\"
+start LaunchLogin_JWT.bat
+cd "C:\HandyCodes\Middleware\APIGatewayDemo\"
+start LaunchGatewayBAT.bat
+cd "C:\HandyCodes\Middleware\ProductsAPIServices\"
+start LaunchProductsBAT.bat
+cd "C:\HandyCodes\Middleware\CustomersAPIServices\"
+start LaunchCustomersBAT.bat
+
